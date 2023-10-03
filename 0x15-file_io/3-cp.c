1b14ace15ef8fc789784f87c9c5f38fab269638d
Adelashraf2491 <adel.ashraf2491@gmailcom>
2023-10-02 17:01:06 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 char *create_buffer(char *file);
-void close_file(int fo);
+void close_file(int fd);
 
 /**
  * create_buffer - IT Allocates 1024 bytes for a buffer.
@@ -28,31 +28,31 @@ char *create_buffer(char *file)
 }
 
 /**
- * close_file - it Closes file descriptors.
- * @fo: The file descriptor to be closed.
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
  */
-void close_file(int fo)
+void close_file(int fd)
 {
 	int c;
 
-	c = close(fo);
+	c = close(fd);
 
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fo %d\n", fo);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
 
 /**
- * main - it Copies the contents of a file to another file.
- * @argc: The number of arguments that was supplied to the program.
- * @argv: it is an array of pointers to the arguments.
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
  * Description: If the argument count is incorrect - exit code 97.
- * If file_from does not exist or not readable - exit code 98.
+ * If file_from does not exist or cannot be read - exit code 98.
  * If file_to cannot be created or written to - exit code 99.
  * If file_to or file_from cannot be closed - exit code 100.
  */
